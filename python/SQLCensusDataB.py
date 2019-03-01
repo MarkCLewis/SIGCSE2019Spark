f214c865b348c3b96809688c3d9e6d25ca636388
Mark Lewis <mlewis@trinity.edu>
2019-03-01 17:27:16 -0600

    Done with Python for a bit. Moving to Java.

--- python/SQLCensusDataB.py
+++ python/SQLCensusDataB.py
@@ -1,7 +1,6 @@
 """SQLCensusDataA.py"""
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField, IntegerType, StringType
-import CensusData
 
 spark = SparkSession.builder.appName("SQL Census Data A").getOrCreate()
 spark.sparkContext.setLogLevel("WARN")
@@ -26,18 +25,15 @@ schema = StructType([ \
 data = spark.read.schema(schema).option("header", True).csv("../data/adult.csv").cache()
 
 n = data.count()
-print("Fraction > 50K = " + str(data.filter(lambda cd: cd.incomeOver50).count() / n))
-print("Average age = " + str(data.map(lambda cd: cd.age).reduce(lambda a, b: a + b) / n))
-over50years = data.filter(lambda cd: cd.age >= 50)
-print("Fraction > 50K in 50+ age group = " + str(over50years.filter(lambda cd: cd.incomeOver50).count() / over50years.count()))
-married = data.filter(lambda cd: cd.maritalStatus == "Married-civ-spouse")
-print("Fraction > 50K in married group = " + str(married.filter(lambda cd: cd.incomeOver50).count() / married.count()))
-print("Quartile age = " + str(data.map(_.age).stat.approxQuantile("value", Array(0.25, 0.5, 0.75), 0.1)))
+print("Fraction > 50K = " + str(data.filter("income = '>50K'").count() / n))
+print("Average age = " + str(data.agg({"age": "sum"}).collect()[0]["sum(age)"] / n))
+over50years = data.filter("age >= 50")
+print("Fraction > 50K in 50+ age group = " + str(over50years.filter("income = '>50K'").count() / over50years.count()))
+married = data.filter("maritalStatus = 'Married-civ-spouse'")
+print("Fraction > 50K in married group = " + str(married.filter("income = '>50K'").count() / married.count()))
+print("Quartile age = " + str(data.select("age").stat.approxQuantile("age", [0.25, 0.5, 0.75], 0.1)))
 print("Fraction by race")
-fracAgg = (FracOver50K).toColumn
-raceCounts = data.groupByKey(lambda cd: cd.race).agg(fracAgg)
-for row in raceCounts.collect():
-    print(row)
-print("Fraction work more than 40 hrs/week = " + data.filter(lambda cd: cd.hoursPerWeek > 40).count() / n)
+# I removed the race grouped fractional computation because it requires using a Pandas UDF.
+print("Fraction work more than 40 hrs/week = " + str(data.filter("hoursPerWeek > 40").count() / n))
 
 spark.stop()
